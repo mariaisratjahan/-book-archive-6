c3544e593603cbedbb2f9b923d965be18658deab
maria israt <68574206+mariaisratjahan@users.noreply.github.com>
2021-09-02 20:24:46 +0600

    done

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 const serchInputFeild=document.getElementById('search-input-feild');
 const cardContainer=document.getElementById('card-container');
-const error=document.getElementById('error');
 const totalSearchItems=document.getElementById('length');
+const error=document.getElementById('error');
 
 const searchFeild=()=>{   
     const searchInputFeildValue=serchInputFeild.value ;
+    // clear total search items........
     totalSearchItems.textContent="";
+
     if(searchInputFeildValue === ''){
         error.innerText="No result found !!!";
         totalSearchItems.style.display="none";
 
     }
     else{
+        // clear error message..............
         error.innerText='';
+        
         url=`https://openlibrary.org/search.json?q=${searchInputFeildValue}`;
         fetch(url)
         .then(res => res.json())
         .then(data => searchResult(data))
         totalSearchItems.style.display="block";
     }
-    // clear
+    // clear previous search results...........
     cardContainer.textContent="";   
   
    
@@ -27,27 +31,30 @@ const searchFeild=()=>{
 const searchResult=(result)=>{
     console.log(result);
     const bookKeeper=result.docs;   
-    totalSearchItems.innerHTML=`<h2 class="search-result">(Total ${bookKeeper.length} results found)</h2>`;
-    
-    console.log(bookKeeper);
-    bookKeeper.forEach(keep =>{
-        console.log(keep);
+    if(bookKeeper.length === 0){
+        error.innerText="No result found !!!";
+    }
+    else{
+        totalSearchItems.innerHTML=`<h2 class="search-result">(Total ${bookKeeper.length} results found)</h2>`;
+        bookKeeper.forEach(keep =>{
         const div=document.createElement('div');
         div.classList.add('col');
         div.innerHTML=`
-            <div class="card h-100">
+            <div class="card h-100 card-details">
                 <img src="https://covers.openlibrary.org/b/id/${keep.cover_i}-M.jpg" class="img-fluid card-img-top" alt="...">
-             <div class="card-body">
+             <div class="card-body card-body ">
                 <h3 class="card-title card-title-style">${keep.title}</h3>
-                <h5 class="orange-color card-text">Author : <small>${keep.author_name}</small></h5>
-                <p><span class="orange-color>Published by :</span> <small>${keep.publisher}</small></p>
-                <p><small class="text-muted">First published year : ${keep?.first_publish_year}</small></p>
+                <h5 class="orange-color card-text">Author : <small>${keep.author_name ? keep.author_name :'no result found'}</small></h5>
+                <p><span class="card-paragraph">Published by :</span> <small>${keep.publisher ? keep.publisher[0]: 'no result found'}</small></p>
+                <p><span class="card-paragraph">First published year :<small class="text-muted"> ${keep.first_publish_year ? keep.first_publish_year : 'no result found'}</small></p>
              </div>
                 
             </div>
         `;
         cardContainer.appendChild(div);
 
-    });
+      });
+    }
+    // clear search feild..............
     serchInputFeild.value="";
 }
